2060ff7730fab7c0554a7d5a12ac9cf1b2e2c9db
GBstudent-AntonianE <saharillo@mail.ru>
2022-10-29 01:58:23 +0300

    new_practical_work1

---
+++ practical_work1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int x = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите число");
+int y = Convert.ToInt32 (Console.ReadLine());
+
+int max = x;
+if (x < y) max = y;
+
+Console.Write("max = ");
+Console.WriteLine(max);
